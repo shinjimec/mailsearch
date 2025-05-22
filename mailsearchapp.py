0ab76be7639020a58b00dd5b9d1dea6af86a44da
shinjimec <shinjimec@gmail.com>
2025-05-22 15:26:00 +0900

    Add files via upload

--- mailsearchapp.py
+++ mailsearchapp.py
@@ -9,8 +9,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import traceback
-import os
-import signal
 
 def web_handler(search_term):
     options = Options()
@@ -26,37 +24,33 @@ def web_handler(search_term):
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     wait = WebDriverWait(driver=browser, timeout=60)
 
-    try:
-        url = 'https://secure.xserver.ne.jp/xapanel/login/xbiz/mail/'
-        browser.get(url)
-        browser.maximize_window()
-        browser.find_element(By.ID,'email').send_keys('gomibako@gomec.co.jp')
-        browser.find_element(By.ID,'mail_password').send_keys('9SQLnZTqy5')
-        browser.find_element(By.XPATH,'//*[@id="login_area"]/div[2]/div/input').click()
+    url = 'https://secure.xserver.ne.jp/xapanel/login/xbiz/mail/'
+    browser.get(url)
+    browser.maximize_window()
+    browser.find_element(By.ID,'email').send_keys('gomibako@gomec.co.jp')
+    browser.find_element(By.ID,'mail_password').send_keys('9SQLnZTqy5')
+    browser.find_element(By.XPATH,'//*[@id="login_area"]/div[2]/div/input').click()
 
-        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="mailIndex"]/div[1]/section/div[1]/div/ul/li[2]/a')))
-        browser.find_element(By.XPATH,'//*[@id="mailIndex"]/div[1]/section/div[1]/div/ul/li[2]/a').click()
+    wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="mailIndex"]/div[1]/section/div[1]/div/ul/li[2]/a')))
+    browser.find_element(By.XPATH,'//*[@id="mailIndex"]/div[1]/section/div[1]/div/ul/li[2]/a').click()
 
-        sleep(2)
-        handles = browser.window_handles
-        browser.switch_to.window(handles[0])
-        browser.close()
-        browser.switch_to.window(handles[1])
+    sleep(2)
+    handles = browser.window_handles
+    browser.switch_to.window(handles[0])
+    browser.close()
+    browser.switch_to.window(handles[1])
 
-        wait.until(EC.presence_of_element_located((By.XPATH, '//a[@id="searchmenulink"]')))
-        browser.find_element(By.XPATH,'//a[@id="searchmenulink"]').click()
-        wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="s_scope_all"]')))
-        browser.find_element(By.XPATH,'//input[@id="s_scope_all"]').click()
+    wait.until(EC.presence_of_element_located((By.XPATH, '//a[@id="searchmenulink"]')))
+    browser.find_element(By.XPATH,'//a[@id="searchmenulink"]').click()
+    wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="s_scope_all"]')))
+    browser.find_element(By.XPATH,'//input[@id="s_scope_all"]').click()
 
-        browser.find_element(By.XPATH,'//a[@id="searchmenulink"]').click()
-        sleep(1)
-        elem_search = browser.find_element(By.ID,'quicksearchbox')
-        elem_search.send_keys(search_term)
-        sleep(4)
-        elem_search.send_keys(Keys.ENTER)
-
-    finally:
-        os.kill(browser.service.process.pid, signal.SIGTERM)
+    browser.find_element(By.XPATH,'//a[@id="searchmenulink"]').click()
+    sleep(1)
+    elem_search = browser.find_element(By.ID,'quicksearchbox')
+    elem_search.send_keys(search_term)
+    sleep(4)
+    elem_search.send_keys(Keys.ENTER)
 
 st.title('迷惑メール検索ツール')
 st.write('検索するワードを入力して[検索]ボタン押してください')
